4218aa1895582dd34af01f28dfce87cefda5e454
Peter Tolstrup <petertolstrup@yahoo.com>
2024-11-13 18:13:36 +0100

    Package updates

--- 02a-Scroll-Indicator-HTML/scripts.js
+++ 02a-Scroll-Indicator-HTML/scripts.js
@@ -1,8 +1,8 @@
 window.addEventListener("scroll", onScroll);
 
 function onScroll() {
-  var scrolled = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolledfactor = (scrolled / height) * 100;
+  const scrolled = document.body.scrollTop || document.documentElement.scrollTop;
+  const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  const scrolledfactor = (scrolled / height) * 100;
   document.querySelector(".scroll_indicator").style.width = `${scrolledfactor}%`;
 }
